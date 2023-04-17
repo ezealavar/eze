a027db5f6f96ae5ed6fd9c07355b5bd3179bd1a9
USUARIO <USUARIO@DESKTOP-Q441P5S>
2023-04-17 19:09:33 -0300

    proyecto tp2 inicial

---
+++ tp2_grupoX-1/src/test/java/ar/edu/unju/fi/Tp2GrupoX1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2GrupoX1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
